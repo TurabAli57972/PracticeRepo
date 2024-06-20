e1c2ce2b99b383c75b49f24d4ffaa85d3f6e9f08
Turab Ali <turabali57972@gmail.com>
2024-06-20 18:07:12 +0500

    Division function added

--- simplecal.go
+++ simplecal.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+func division(num1 float64, num2 float64) float64 {
+	return num1 / num2
+}
 func main() {
 	fmt.Println(division(506, 22))
 	fmt.Println(add(360, 290))
