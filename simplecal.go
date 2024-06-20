b60b59aabbc21d7a692a2030e7e15e34818a09c0
Turab Ali <turabali57972@gmail.com>
2024-06-20 18:11:09 +0500

    Addition function added

--- simplecal.go
+++ simplecal.go
@@ -5,6 +5,10 @@ import "fmt"
 func division(num1 float64, num2 float64) float64 {
 	return num1 / num2
 }
+func add(num1 float64, num2 float64) float64 {
+	return num1 + num2
+}
+
 func main() {
 	fmt.Println(division(506, 22))
 	fmt.Println(add(360, 290))
